e0c83361be743f377ed78bc9f65aead436cdc972
kamal <maroc.develop@gmail.com>
2024-01-07 13:59:37 +0100

    .

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import '../app.css';
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-center p-12">
+<div class="flex min-h-screen flex-col items-center justify-center p-6">
 	<slot />
 </div>

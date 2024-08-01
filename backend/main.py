906c008a7266afc5bc5d43e26e01b8e036ccb58a
kamal <maroc.develop@gmail.com>
2024-08-01 14:19:40 +0100

    update cors

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 """high level support for doing this and that."""
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 # Router
@@ -9,22 +9,45 @@ from app.routers import answer, search_stream
 
 app = FastAPI()
 
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "https://0.0.0.0:3000",
+#     "http://0.0.0.0:3000",
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "https://0.0.0.0:3000",
-    "http://0.0.0.0:3000",
+    "https://zeia.ma",
+    "https://www.zeia.ma",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
 
 app.include_router(search_stream.router, prefix="/search_stream")
 app.include_router(answer.router, prefix="/answer")
